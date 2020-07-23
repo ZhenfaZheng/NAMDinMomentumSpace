0ce2256a417db90e917dcb8c88950e9bbd0907a9
ZhenfaZheng <zhenfacn@gmail.com>
2020-07-23 19:25:21 +0800

    Amplify dimension of basis by number of k-points.
    NBASIS multiply by NKPOINTS.

--- fileio.f90
+++ fileio.f90
@@ -155,7 +155,7 @@ module fileio
       ! assign the parameters
       inp%BMIN     = bmin
       inp%BMAX     = bmax
-      inp%NBASIS   = bmax - bmin + 1
+      inp%NBASIS   = (bmax - bmin + 1) * nkpoints
       inp%NSW      = nsw
       inp%NBANDS   = nbands
       inp%NKPOINTS   = nkpoints
