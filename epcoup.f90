6e05dcfe5b250aaa0db7eef87e5c85618e277eca
ZhenfaZheng <zhenfacn@gmail.com>
2020-07-23 21:38:57 +0800

    Add epcoup module.

---
+++ epcoup.f90
@@ -0,0 +1,15 @@
+module epcoup
+  use fileio
+
+  implicit none
+
+  subroutine ReadEPC(inp, epcq)
+  end subroutine
+
+  subroutine EPCq2r(epcq, epcr)
+  end subroutine
+
+  subroutine TDepCoupIJ(inp, olap)
+  end subroutine
+
+end module epcoup
