ec89257df25d19c7b85ef76d30d2ddfc95ae7e1f
ZhenfaZheng <zhenfacn@gmail.com>
2023-07-11 15:31:45 +0800

    Modify version tag.

--- src/main.f90
+++ src/main.f90
@@ -24,7 +24,7 @@ Program main
   integer :: ns
 
   write(*,*)
-  write(*,*) "Hefei-NAMD (epc version 2.1.10, Jul 06, 2023)"
+  write(*,*) "Hefei-NAMD (epc version 2.1.11, Jul 06, 2023)"
 
   call MPI_INIT(ierr)
   call MPI_COMM_RANK(MPI_COMM_WORLD, irank, ierr)
